afe0597de7bfebe792456c5ebf94c72bc07c5845
Julian <you@example.com>
2020-03-19 16:33:40 -0500

    update the menu-items to navigate wihRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import "./menu-item.styles.scss"
+import React from "react";
+import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({title, imageUrl, size}) => {
-    return  <div className={`${size} menu-item`} >
-    {/* // We dont want the image be bigger when it scales so we make this dive,
-    we just want i bigger when one hovers over it */}
-    <div className="background-image" style={{
-        backgroundImage: `url(${imageUrl})`
-    }}/>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      {/* We dont want the image to be bigger when it scales so we make this div,
+    we just want it bigger when one hovers over it */}
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }} /* If we wrap the rest of the elemens they will also scale and we DONT want that */
+      />
 
-    <div className="content">
+      <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-</div>
-}
+  );
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
